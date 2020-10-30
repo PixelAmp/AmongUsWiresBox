3e54166903ca3a8c25f658bb1c838b74bae7f239
Pixel Amp <reperez79@gmail.com>
2020-10-30 02:06:09 -0700

    Remove unused sequences, add startup and success

--- AmongUsBox.py
+++ AmongUsBox.py
@@ -1,94 +1,84 @@
 import RPi.GPIO as GPIO
 import time
 
-def Standalone(LEDpins, BUTTONpins, interval):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    for slot in range(4):
-        GPIO.setup(LEDpins[slot], GPIO.OUT)
-        GPIO.setup(BUTTONpins[slot], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    while True:
-        print("loop")
-        #LED stuff
+def successSequence(LEDpins, GPIO):
+    interval = .25
+    while interval > 0:
         GPIO.output(LEDpins[0], GPIO.HIGH)
+        time.sleep(interval)
         GPIO.output(LEDpins[1], GPIO.HIGH)
+        time.sleep(interval)
         GPIO.output(LEDpins[2], GPIO.HIGH)
+        time.sleep(interval)
         GPIO.output(LEDpins[3], GPIO.HIGH)
         time.sleep(interval)
-        
+
         GPIO.output(LEDpins[0], GPIO.LOW)
+        time.sleep(interval)
         GPIO.output(LEDpins[1], GPIO.LOW)
+        time.sleep(interval)
         GPIO.output(LEDpins[2], GPIO.LOW)
+        time.sleep(interval)
         GPIO.output(LEDpins[3], GPIO.LOW)
         time.sleep(interval)
 
-        #button stuff
-        cable0 = GPIO.input(BUTTONpins[0])
-        cable1 = GPIO.input(BUTTONpins[1])
-        cable2 = GPIO.input(BUTTONpins[2])
-        cable3 = GPIO.input(BUTTONpins[3])
-        if cable0 == False:
-            print('Cable 0 connected')
-        if cable1 == False:
-            print('Cable 1 connected')
-        if cable2 == False:
-            print('Cable 2 connected')
-        if cable3 == False:
-            print('Cable 3 connected')
-        
-        time.sleep(interval)
+        interval -= .05
+    
+    return
+
+def startupSequence(LEDpins, GPIO):
+
+    GPIO.output(LEDpins[0], GPIO.HIGH)
+    GPIO.output(LEDpins[1], GPIO.HIGH)
+    GPIO.output(LEDpins[2], GPIO.HIGH)
+    GPIO.output(LEDpins[3], GPIO.HIGH)
+    time.sleep(1)
+
+    GPIO.output(LEDpins[0], GPIO.LOW)
+    GPIO.output(LEDpins[1], GPIO.LOW)
+    GPIO.output(LEDpins[2], GPIO.LOW)
+    GPIO.output(LEDpins[3], GPIO.LOW)
+    time.sleep(.5)
+
+    successSequence(LEDpins, GPIO)
+
+    GPIO.output(LEDpins[0], GPIO.HIGH)
+    GPIO.output(LEDpins[1], GPIO.HIGH)
+    GPIO.output(LEDpins[2], GPIO.HIGH)
+    GPIO.output(LEDpins[3], GPIO.HIGH)
+    time.sleep(1)
+
+    GPIO.output(LEDpins[0], GPIO.LOW)
+    GPIO.output(LEDpins[1], GPIO.LOW)
+    GPIO.output(LEDpins[2], GPIO.LOW)
+    GPIO.output(LEDpins[3], GPIO.LOW)
+    time.sleep(1)
+
+    print("Startup successful!")
+    return
 
 
-def CablesLightLEDs(LEDpins, BUTTONpins, interval):
+def main():
+    print("Starting!")
+    LEDpins = [5,13,21,25]      # The pins for LED's
+    BUTTONpins = [4,17,27,22]    # The The Pins for the wires
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     for slot in range(4):
         GPIO.setup(LEDpins[slot], GPIO.OUT)
         GPIO.setup(BUTTONpins[slot], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    while True:
-        cable0 = GPIO.input(BUTTONpins[0])
-        cable1 = GPIO.input(BUTTONpins[1])
-        cable2 = GPIO.input(BUTTONpins[2])
-        cable3 = GPIO.input(BUTTONpins[3])
-        
-        
-        GPIO.output(LEDpins[0], GPIO.HIGH) if cable0 == False else GPIO.output(LEDpins[0], GPIO.LOW)
-        GPIO.output(LEDpins[1], GPIO.HIGH) if cable0 == False else GPIO.output(LEDpins[1], GPIO.LOW)
-        GPIO.output(LEDpins[2], GPIO.HIGH) if cable0 == False else GPIO.output(LEDpins[2], GPIO.LOW)
-        GPIO.output(LEDpins[3], GPIO.HIGH) if cable0 == False else GPIO.output(LEDpins[3], GPIO.LOW)
 
-def cleanLoop(LEDpins, BUTTONpins, interval):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    for slot in range(4):
-        GPIO.setup(LEDpins[slot], GPIO.OUT)
-        GPIO.setup(BUTTONpins[slot], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    startupSequence(LEDpins, GPIO)
+
     while True:
         for slot in range(4):
             cable = GPIO.input(BUTTONpins[slot])
             GPIO.output(LEDpins[slot], GPIO.HIGH) if cable == False else GPIO.output(LEDpins[slot], GPIO.LOW)
 
-
-
-def main():
-    print("starting!")
-    LEDpins = [5,13,21,25]      # The pins for LED's
-    BUTTONpins = [4,17,27,22]    # The The Pins for the wires
-    interval = .2               # The length of time to blink on or off
-    
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setwarnings(False)
-    # for slot in range(4):
-    #     GPIO.setup(LEDpins[slot], GPIO.OUT)
-    #     GPIO.setup(BUTTONpins[slot], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    #Standalone(LEDpins, BUTTONpins, interval)
-    #CablesLightLEDs(LEDpins, BUTTONpins, interval)
-    cleanLoop(LEDpins, BUTTONpins, interval)
-
-
-
-
+            if GPIO.output(LEDpins[0], GPIO.HIGH) and GPIO.output(LEDpins[1], GPIO.HIGH) and GPIO.output(LEDpins[2], GPIO.HIGH) and GPIO.output(LEDpins[3], GPIO.HIGH):
+                time.sleep(.5)
+                successSequence(LEDpins, GPIO)
 
 if __name__ == "__main__":
     main()
