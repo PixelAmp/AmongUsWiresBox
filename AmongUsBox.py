3903f27d7ac670893c41373234ac537482186ab7
Pixel Amp <reperez79@gmail.com>
2020-10-30 17:06:51 -0700

    Adjust pin order, force GPIO low

--- AmongUsBox.py
+++ AmongUsBox.py
@@ -49,10 +49,13 @@ def startupSequence(LEDpins, GPIO):
     time.sleep(1)
 
     GPIO.output(LEDpins[0], GPIO.LOW)
+    time.sleep(.1)
     GPIO.output(LEDpins[1], GPIO.LOW)
+    time.sleep(.1)
     GPIO.output(LEDpins[2], GPIO.LOW)
+    time.sleep(.1)
     GPIO.output(LEDpins[3], GPIO.LOW)
-    time.sleep(1)
+    time.sleep(1.1)
 
     print("Startup successful!")
     return
@@ -60,8 +63,8 @@ def startupSequence(LEDpins, GPIO):
 
 def main():
     print("Starting!")
-    LEDpins = [5,13,21,25]      # The pins for LED's
-    BUTTONpins = [4,17,27,22]    # The The Pins for the wires
+    LEDpins = [5,13,21,25]      # The Pins for LED's
+    BUTTONpins = [22,21,17,4]    # The The Pins for the wires
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
@@ -70,11 +73,16 @@ def main():
         GPIO.setup(BUTTONpins[slot], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     startupSequence(LEDpins, GPIO)
+    
+    GPIO.output(LEDpins[0], GPIO.LOW)
+    GPIO.output(LEDpins[1], GPIO.LOW)
+    GPIO.output(LEDpins[2], GPIO.LOW)
+    GPIO.output(LEDpins[3], GPIO.LOW)
 
     while True:
         for slot in range(4):
             cable = GPIO.input(BUTTONpins[slot])
-            GPIO.output(LEDpins[slot], GPIO.HIGH) if cable == False else GPIO.output(LEDpins[slot], GPIO.LOW)
+            GPIO.output(LEDpins[slot], GPIO.HIGH) if cable == True else GPIO.output(LEDpins[slot], GPIO.LOW)
 
             if GPIO.output(LEDpins[0], GPIO.HIGH) and GPIO.output(LEDpins[1], GPIO.HIGH) and GPIO.output(LEDpins[2], GPIO.HIGH) and GPIO.output(LEDpins[3], GPIO.HIGH):
                 time.sleep(.5)
